e2e5f6d95304cf9a7b3accdbd464fa4e4d476fb0
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-01-23 07:47:35 -0700

    Week 3 - Day 1

---
+++ TargetCSharp/Week3/Program.cs
@@ -0,0 +1,16 @@
+﻿string name;
+DateTime birthdate;
+int ageInDay;
+
+Console.Write("What is your name? ");
+name  = Console.ReadLine();
+
+Console.Write("What is your birthday (yyyy-mm-dd)? ");
+birthdate = DateTime.Parse(Console.ReadLine());
+ageInDay = (DateTime.Now - birthdate).Days;
+
+Console.WriteLine(name + " your birthday of " + birthdate.ToShortDateString() + " is " + ageInDay + " days old");
+Console.WriteLine($"{name} your birthday of {birthdate} is {ageInDay} days old");
+//  showing that without the "$" sign, the output is just a text
+Console.WriteLine("{name} your birthday of {birthdate} is {ageInDay} days old");
+Console.ReadKey();
