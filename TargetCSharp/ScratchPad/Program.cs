31ea4cbe769ac310e326e7c01a0d7456df7610a5
James Thompson <jthompson@nait.ca>
2024-02-09 09:00:52 -0700

    Checkin

---
+++ TargetCSharp/ScratchPad/Program.cs
@@ -0,0 +1,20 @@
+﻿int newScore = 88;
+
+switch (newScore)
+{
+    case int currentScore when currentScore >= 90:
+        Console.WriteLine("Grade: A");
+        break;
+    case int currentScore when currentScore >= 80:
+        Console.WriteLine("Grade: B");
+        break;
+    case int currentScore when currentScore >= 70:
+        Console.WriteLine("Grade: C");
+        break;
+    case int currentScore when currentScore >= 60:
+        Console.WriteLine("Grade: D");
+        break;
+    default:
+        Console.WriteLine("Grade: F");
+        break;
+}
