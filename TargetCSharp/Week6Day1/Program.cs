d0478faa64fb50d5d4ed7595cd4d1a6678a37ce9
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-02-13 05:48:41 -0700

    Added Week 6 Day Problem 1

---
+++ TargetCSharp/Week6Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿//1. Find the sum of the squares of the integers from 1 to MySquare, where MySquare is input by the 
+//user. E.g. user enters 4 then return 1x1 + 2x2 + 3x3 + 4x4 = 30
+int num = 0;
+int num2 = 0;
+int squareValue = 0;
+Console.Write("Please enter in a number: ");
+num = int.Parse(Console.ReadLine());
+num2 = num;
+string runningValues = "$User enters {num} then return ";
+while (num > 0)
+{
+    squareValue = squareValue + num * num;
+    runningValues = runningValues + $"{num} * {num}";
+    num = num - 1;
+    if (num != 0)
+    {
+        runningValues = runningValues + " + ";
+    }
+}
+Console.WriteLine($"Square Total = {squareValue}");
+Console.WriteLine($"{runningValues} = {squareValue}");
+Console.WriteLine();
+Console.WriteLine();
+squareValue = 0;
+do
+{
+    squareValue = squareValue + num2 * num2;
+    num2 = num2 - 1;
+} while (num2 > 0);
+Console.Write($"Square Total 2 = {squareValue}");
